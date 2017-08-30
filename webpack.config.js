ca3f7a655b1fdd94618d7233a9c7964b872f09ba
Dan Rosén <dan.rosen@gu.se>
2017-08-30 15:59:08 +0200

    Rename index.tsx to index.ts

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require("path");
 
 module.exports = {
     entry: [
-        "./src/index.tsx",
+        "./src/index.ts",
     ],
     output: {
         path: path.join(__dirname, 'dist'),
