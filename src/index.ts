3b2e3dc30841a1ab7c8599df047866ef713e946b
Dan Rosén <dan.rosen@gu.se>
2017-09-18 14:45:31 +0200

    Load test only on #test

--- src/index.ts
+++ src/index.ts
@@ -8,11 +8,46 @@ const root = document.getElementById('root') as HTMLElement
 
 const hash_text = window.location.hash.slice(1)
 
+
 const example_text = hash_text || "En dag jag vaknade när larmet på min telefon ringde. De väder var inte fint."
 
 let View = require('./View')
 
-let get = View.bind(root, View.init_data(example_text))
+const init = hash_text != "test" ? View.init_data(example_text) : {
+  now: {
+    tokens: ['aaa ', 'abc ', 'ghi ', 'def '],
+    spans: [
+      {
+        "text": "rst ",
+        "labels": [],
+        "links": [],
+        "moved": false
+      },
+      {
+        "text": "aba ",
+        "labels": [],
+        "links": [ 0 ],
+        "moved": false
+      },
+      {
+        "text": "jkl ",
+        "labels": [],
+        "links": [],
+        "moved": false
+      },
+      {
+        "text": "dexhi ",
+        "labels": [],
+        "links": [ 3, 2 ],
+        "moved": true
+      }
+    ]
+  },
+  past: [],
+  future: []
+}
+
+let get = View.bind(root, init)
 
 // could retain history, but we reinitialize the data here
 window.onhashchange = () => {
