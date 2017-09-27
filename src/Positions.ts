39c08737d91c24b7c5a7eb1f67aea6d797bac9bb
Dan Rosén <dan.rosen@gu.se>
2017-09-27 10:04:16 +0200

    Remove unused functions in Positions

--- src/Positions.ts
+++ src/Positions.ts
@@ -34,7 +34,6 @@ const update = (id: string, d: PosDict, x: HTMLElement) => {
     height: x.offsetHeight
   }
   if (!(id in d.dict) || !eq_pos(p, d.dict[id])) {
-    //console.log('updating', id, 'to', Utils.show(p))
     d.modified = true
     d.dict[id] = p
   }
@@ -56,53 +55,6 @@ export const posid = (id: string, d: PosDict, v: VNode) => ({
     }
   }
 })
-
-function span<A>(xs: A[], p: (x: A) => boolean): [A[], A[]] {
-  for (let i = 0; i < xs.length; i++) {
-    if (!p(xs[i])) {
-      return [xs.slice(0, i), xs.slice(i)]
-    }
-  }
-  return [xs, []]
-}
-
-function span_end<A>(xs: A[], p: (x: A) => boolean): [A[], A[]] {
-  for (let i = xs.length - 1; i >= 0; i--) {
-    if (!p(xs[i])) {
-      return [xs.slice(0, i+1), xs.slice(i+1)]
-    }
-  }
-  return [[], xs]
-}
-
-function trim<A>(xs: A[], p: (x: A) => boolean): [A[], A[], A[]] {
-  const [p_init, ys] = span(xs, p)
-  const [not_p, p_tail] = span_end(ys, p)
-  return [p_init, not_p, p_tail]
-}
-
-export function posid_ignore_child(id: string, d: PosDict, v: VNode, ignore_class: string): VNode {
-  const ignore = (n: VNode | string) => {
-    if (typeof n === 'string') {
-      return n.trim() == ''
-    } else if (n.data && n.data.class && n.data.class[ignore_class]) {
-      return true
-    } else if (n.text != null && n.text.trim() == '') {
-      return true
-    }
-    return false
-  }
-  if (v.children && v.children.length >= 2) {
-    const [ignore_init, ok, ignore_tail] = trim(v.children, ignore)
-    if (ignore_init.length + ignore_tail.length >= 1 && ok.length >= 1) {
-      return {
-        ...v,
-        children: [...ignore_init, posid(id, d, h('span', ok)), ...ignore_tail]
-      }
-    }
-  }
-  return posid(id, d, v)
-}
 
 export function relative(n1: VNode, n2: VNode, classes: string[] = []): VNode {
   return (
