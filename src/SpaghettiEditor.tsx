878b583cbca629b2a5a0b9af415c60a12a7da0b3
Dan Rosén <dan.rosen@gu.se>
2018-04-11 11:07:58 +0200

    Introduce SpaghettiEditorConfig

--- src/SpaghettiEditor.tsx
+++ src/SpaghettiEditor.tsx
@@ -27,14 +27,7 @@ import 'codemirror/lib/codemirror.css'
 import 'lato-font/css/lato-font.min.css'
 import 'dejavu-fonts-ttf/ttf/DejaVuSans.ttf'
 
-const order_changing_labels: Record<string, true> = {
-  O: true,
-  WO: true,
-  INV: true,
-  OINV: true,
-}
-
-export const config = {order_changing_labels}
+import {config} from './SpaghettiEditorConfig'
 
 export interface State {
   readonly graph: Undo<Graph>
@@ -188,55 +181,6 @@ function LabelSidekick({store, onBlur}: {store: Store<State>; onBlur: () => void
   }
   return null
 }
-
-interface ex {
-  source: string
-  target: string
-}
-const ex = (source: string, target: string): ex => ({source, target})
-
-const examples: ex[] = `
-Their was a problem yesteray . // There was a problem yesterday .
-
-The team that hits the most runs get ice cream . // The team that hits the most runs gets ice cream .
-
-Blue birds have blue and pink feathers . // Bluebirds have blue and pink feathers .
-
-I don't know his lives . // I don't know where he~his lives .
-
-He get to cleaned his son . // He got his~his son~son to:O clean:O the~ room~ .
-
-We wrote down the number . // We wrote the number down~down .
-
-English is my second language with many difficulties that I faced them in
-twolast years I moved in United States .
-//
-
-In my homeland we didn’t write as structural as here .
-//
-
-During the semester , I frustrated many times with my grades and thought I
-couldn’t go up any more , because there was a very big difference between
-ESOL 40 with other language people .
-//
-
-Sometimes , I recognized about why I’m here and studying with this crazy
-language that I couldn’t be good at all .
-//
-
-In contrast I faced with my beliefs and challenges that helped me to focus
-on my mind to write an essay with these difficult subjectswith no experience
-as narrative essay , business essay and all argumentative essays .
-//
-
-It makes me proud of myself to write something I never thought I can do
-in end of this semester and improve my writing skills , have learned my
-challenges and discovered strategies to overcome the challenges .
-//
-`
-  .trim()
-  .split(/\n\n+/gm)
-  .map(line => ex.apply({}, line.split('//').map(side => side.trim())))
 
 const topStyle = style({
   ...Utils.debugName('topStyle'),
@@ -512,7 +456,7 @@ export function View(store: Store<State>, cm_target: CM.CMVN): VNode {
         <div className="main TopPad">
           <em>Examples:</em>
         </div>
-        {examples.map((e, i) => (
+        {config.examples.map((e, i) => (
           <React.Fragment key={i}>
             {!e.target ? (
               <div />
