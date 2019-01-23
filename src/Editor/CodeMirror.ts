e1525760e354edb82c213fef456a71e738f61e4a
Arild Matsson <arild@klavaro.se>
2019-01-23 14:59:45 +0100

    Avoid setting text from local storage?

--- src/Editor/CodeMirror.ts
+++ src/Editor/CodeMirror.ts
@@ -74,7 +74,6 @@ export function GraphEditingCM(
 
   const {cm, node} = CM({extraKeys, tabindex: 3})
   defaultTabBehaviour(cm)
-  cm.setValue(G.get_side_text(Model.viewGraph(store), side))
 
   const {Index} = PositionUtils(cm, store, side)
 
