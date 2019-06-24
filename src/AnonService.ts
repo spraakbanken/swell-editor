f1a983052221a45110f2f5b28da3aae8bea6b335
Arild Matsson <arild.matsson@gu.se>
2019-06-24 12:16:11 +0200

    Pseuws name

--- src/AnonService.ts
+++ src/AnonService.ts
@@ -3,32 +3,39 @@ import * as Utils from './Utils'
 import {Store} from 'reactive-lens'
 import {Pseudonyms, is_anon_label} from './Editor/Anonymization'
 
-const URL = 'https://ws.spraakbanken.gu.se/ws/larka/anon'
+const URL = 'https://ws.spraakbanken.gu.se/ws/larka/pseuws'
 
-type AnonText = {string: string; label: string[]}[]
+type PseuwsText = {string: string; label: string[]}[]
 
 export function anonService(graphStore: Store<G.Graph>, pseudonymsStore: Store<Pseudonyms>): void {
-  let graph = graphStore.get()
   Utils.request(
     URL,
-    {method: 'POST', data: `text=${encodeURIComponent(G.source_text(graph))}`},
+    {method: 'POST', data: `text=${encodeURIComponent(G.source_text(graphStore.get()))}`},
     response => {
-      const toks: AnonText = [].concat(...JSON.parse(response))
-      if (toks.length != graph.source.length) {
-        console.error(toks.length, toks)
-        Utils.raise('Pseudonymizer result has wrong length')
-      }
-
-      const em = G.edge_map(graph)
-      toks.forEach((tok, i) => {
-        const clean_labels = tok.label.map(l => labelMap[l] || l).filter(is_anon_label)
-        const e = Utils.getUnsafe(em, graph.source[i].id)
-        graph = G.modify_labels(graph, e.id, labels => clean_labels)
-        tok.label.length && pseudonymsStore.update({[clean_labels.join(' ')]: tok.string})
-      })
+      const toks: PseuwsText = [].concat(...JSON.parse(response))
+      const {graph, pseudonyms} = applyPseuws(graphStore.get(), toks)
+      pseudonymsStore.update(pseudonyms)
       graphStore.set(graph)
     }
   )
+}
+
+export function applyPseuws(graph: G.Graph, toks: PseuwsText) {
+  if (toks.length != graph.source.length) {
+    console.error(toks.length, toks)
+    Utils.raise('Pseudonymizer result has wrong length')
+  }
+
+  const pseudonyms: Pseudonyms = {}
+  const em = G.edge_map(graph)
+  toks.forEach((tok, i) => {
+    const clean_labels = tok.label.map(l => labelMap[l] || l).filter(is_anon_label)
+    const e = Utils.getUnsafe(em, graph.source[i].id)
+    graph = G.modify_labels(graph, e.id, labels => clean_labels)
+    clean_labels && (pseudonyms[clean_labels.join(' ')] = tok.string)
+  })
+
+  return {graph, pseudonyms}
 }
 
 // TODO: Temporary.
